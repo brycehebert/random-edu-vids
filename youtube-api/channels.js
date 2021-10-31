5ead1c038e917dc135df81897eef905cf9bd093b
Bryce Hebert <brycehebert@users.noreply.github.com>
2021-10-31 08:09:20 -0500

    Move channel info to own file

---
+++ youtube-api/channels.js
@@ -0,0 +1,30 @@
+module.exports.channels = {
+  cgpgrey: {
+    channelID: "UC2C_jShtL725hvbm1arSV9w",
+    uploads: "UU2C_jShtL725hvbm1arSV9w"
+  },
+  vsauce: {
+    channelID: "UC6nSFpj9HTCZ5t-N3Rm3-HA",
+    uploads: "UU6nSFpj9HTCZ5t-N3Rm3-HA"
+  },
+  veritasium: {
+    channelID: "UCHnyfMqiRRG1u-2MsSQLbXA",
+    uploads: "UUHnyfMqiRRG1u-2MsSQLbXA"
+  },
+  smartereveryday: {
+    channelID: "UC6107grRI4m0o2-emgoDnAA",
+    uploads: "UU6107grRI4m0o2-emgoDnAA"
+  },
+  markrober: {
+    channelID: "UCY1kMZp36IQSyNx_9h4mpCg",
+    uploads: "UUY1kMZp36IQSyNx_9h4mpCg"
+  },
+  nilered: {
+    channelID: "UCFhXFikryT4aFcLkLw2LBLA",
+    uploads: "UUFhXFikryT4aFcLkLw2LBLA"
+  },
+  realengineering: {
+    channelID: "UCR1IuLEqb6UEA_zQ81kwXfg",
+    uploads: "UUR1IuLEqb6UEA_zQ81kwXfg"
+  }
+};

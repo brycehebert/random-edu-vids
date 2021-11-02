fc53dc3cc6de4f3774cd947463879c349c6e48f3
Bryce Hebert <brycehebert@users.noreply.github.com>
2021-11-02 01:47:28 -0500

    move json files to own directory

--- youtube-api/youtube-api.js
+++ youtube-api/youtube-api.js
@@ -75,7 +75,7 @@ const getAndWritePlaylist = (channelName) => {
 
 //Write the given playlist to a json file. Use name to name the file name-uploads.json
 const writePlaylist = (name, playlist) => {
-  fs.writeFileSync(`${__dirname}/${name}-uploads.json`, JSON.stringify(playlist));
+  fs.writeFileSync(`${__dirname}/playlists/${name}-uploads.json`, JSON.stringify(playlist));
 };
 
 getAndWritePlaylist("cgpgrey");

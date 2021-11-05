19be0ecceb26548359e45fdfaa74f94910c84d2c
Bryce Hebert <brycehebert@users.noreply.github.com>
2021-11-05 08:09:21 -0500

    data for dev purposes

---
+++ client/src/seed.js
@@ -0,0 +1,302 @@
+//Temporary data for dev purposes
+
+export const videos = [
+  {
+    publishedAt: "2021-10-27T17:31:35Z",
+    title: "Murdered by Magic, for Mom",
+    thumbnails: {
+      default: {
+        url: "https://i.ytimg.com/vi/uhzf9j-iCPs/default.jpg",
+        width: 120,
+        height: 90
+      },
+      medium: {
+        url: "https://i.ytimg.com/vi/uhzf9j-iCPs/mqdefault.jpg",
+        width: 320,
+        height: 180
+      }
+    },
+    channelTitle: "CGP Grey",
+    resourceId: {
+      videoId: "uhzf9j-iCPs"
+    }
+  },
+  {
+    publishedAt: "2021-09-09T06:16:32Z",
+    title: "Someone Dead Ruined My Life… Again.",
+    thumbnails: {
+      default: {
+        url: "https://i.ytimg.com/vi/qEV9qoup2mQ/default.jpg",
+        width: 120,
+        height: 90
+      },
+      medium: {
+        url: "https://i.ytimg.com/vi/qEV9qoup2mQ/mqdefault.jpg",
+        width: 320,
+        height: 180
+      }
+    },
+    channelTitle: "CGP Grey",
+    resourceId: {
+      videoId: "qEV9qoup2mQ"
+    }
+  },
+  {
+    publishedAt: "2021-09-08T08:46:04Z",
+    title: "Physical Characteristics of the Tiffanys",
+    thumbnails: {
+      default: {
+        url: "https://i.ytimg.com/vi/ZhCAbDeLgTw/default.jpg",
+        width: 120,
+        height: 90
+      },
+      medium: {
+        url: "https://i.ytimg.com/vi/ZhCAbDeLgTw/mqdefault.jpg",
+        width: 320,
+        height: 180
+      }
+    },
+    channelTitle: "CGP Grey",
+    resourceId: {
+      videoId: "ZhCAbDeLgTw"
+    }
+  },
+  {
+    publishedAt: "2021-08-09T08:23:06Z",
+    title: "💖 The Tale of Tiffany 💖",
+    thumbnails: {
+      default: {
+        url: "https://i.ytimg.com/vi/9LMr5XTgeyI/default.jpg",
+        width: 120,
+        height: 90
+      },
+      medium: {
+        url: "https://i.ytimg.com/vi/9LMr5XTgeyI/mqdefault.jpg",
+        width: 320,
+        height: 180
+      }
+    },
+    channelTitle: "CGP Grey",
+    resourceId: {
+      videoId: "9LMr5XTgeyI"
+    }
+  },
+  {
+    publishedAt: "2021-07-12T16:37:43Z",
+    title: "Getting Vaccinated at the Coolest Place 😎",
+    thumbnails: {
+      default: {
+        url: "https://i.ytimg.com/vi/xJmVuW8atO8/default.jpg",
+        width: 120,
+        height: 90
+      },
+      medium: {
+        url: "https://i.ytimg.com/vi/xJmVuW8atO8/mqdefault.jpg",
+        width: 320,
+        height: 180
+      }
+    },
+    channelTitle: "CGP Grey",
+    resourceId: {
+      videoId: "xJmVuW8atO8"
+    }
+  },
+  {
+    publishedAt: "2021-05-23T18:38:44Z",
+    title: "The Battle of SHARKS!",
+    thumbnails: {
+      default: {
+        url: "https://i.ytimg.com/vi/_8xhdL8BPvU/default.jpg",
+        width: 120,
+        height: 90
+      },
+      medium: {
+        url: "https://i.ytimg.com/vi/_8xhdL8BPvU/mqdefault.jpg",
+        width: 320,
+        height: 180
+      }
+    },
+    channelTitle: "CGP Grey",
+    resourceId: {
+      videoId: "_8xhdL8BPvU"
+    }
+  },
+  {
+    publishedAt: "2021-05-14T17:25:36Z",
+    title: "Grey Tries to Get Back into Magic: The Gathering",
+    thumbnails: {
+      default: {
+        url: "https://i.ytimg.com/vi/GJWtum6I_jo/default.jpg",
+        width: 120,
+        height: 90
+      },
+      medium: {
+        url: "https://i.ytimg.com/vi/GJWtum6I_jo/mqdefault.jpg",
+        width: 320,
+        height: 180
+      }
+    },
+    channelTitle: "CGP Grey",
+    resourceId: {
+      videoId: "GJWtum6I_jo"
+    }
+  },
+  {
+    publishedAt: "2021-03-30T15:39:24Z",
+    title: "Grey Plays: Myst",
+    thumbnails: {
+      default: {
+        url: "https://i.ytimg.com/vi/_cpa0PNKO-c/default.jpg",
+        width: 120,
+        height: 90
+      },
+      medium: {
+        url: "https://i.ytimg.com/vi/_cpa0PNKO-c/mqdefault.jpg",
+        width: 320,
+        height: 180
+      }
+    },
+    channelTitle: "CGP Grey",
+    resourceId: {
+      videoId: "_cpa0PNKO-c"
+    }
+  },
+  {
+    publishedAt: "2021-05-10T21:12:04Z",
+    title: "A Crime Against Childhood",
+    thumbnails: {
+      default: {
+        url: "https://i.ytimg.com/vi/-FBwZtuJtMw/default.jpg",
+        width: 120,
+        height: 90
+      },
+      medium: {
+        url: "https://i.ytimg.com/vi/-FBwZtuJtMw/mqdefault.jpg",
+        width: 320,
+        height: 180
+      }
+    },
+    channelTitle: "CGP Grey",
+    resourceId: {
+      videoId: "-FBwZtuJtMw"
+    }
+  },
+  {
+    publishedAt: "2021-04-17T13:50:57Z",
+    title: "Grey Plays Minecraft",
+    thumbnails: {
+      default: {
+        url: "https://i.ytimg.com/vi/tdRrPdSxhMI/default.jpg",
+        width: 120,
+        height: 90
+      },
+      medium: {
+        url: "https://i.ytimg.com/vi/tdRrPdSxhMI/mqdefault.jpg",
+        width: 320,
+        height: 180
+      }
+    },
+    channelTitle: "CGP Grey",
+    resourceId: {
+      videoId: "tdRrPdSxhMI"
+    }
+  },
+  {
+    publishedAt: "2021-03-26T19:45:13Z",
+    title: "Metric Paper",
+    thumbnails: {
+      default: {
+        url: "https://i.ytimg.com/vi/pUF5esTscZI/default.jpg",
+        width: 120,
+        height: 90
+      },
+      medium: {
+        url: "https://i.ytimg.com/vi/pUF5esTscZI/mqdefault.jpg",
+        width: 320,
+        height: 180
+      }
+    },
+    channelTitle: "CGP Grey",
+    resourceId: {
+      videoId: "pUF5esTscZI"
+    }
+  },
+  {
+    publishedAt: "2021-02-27T19:24:09Z",
+    title: "Ten Years Later",
+    thumbnails: {
+      default: {
+        url: "https://i.ytimg.com/vi/qC5h9zcHmPA/default.jpg",
+        width: 120,
+        height: 90
+      },
+      medium: {
+        url: "https://i.ytimg.com/vi/qC5h9zcHmPA/mqdefault.jpg",
+        width: 320,
+        height: 180
+      }
+    },
+    channelTitle: "CGP Grey",
+    resourceId: {
+      videoId: "qC5h9zcHmPA"
+    }
+  },
+  {
+    publishedAt: "2021-02-19T17:02:23Z",
+    title: "Escape from Lockdown!",
+    thumbnails: {
+      default: {
+        url: "https://i.ytimg.com/vi/FWP41MXLMGY/default.jpg",
+        width: 120,
+        height: 90
+      },
+      medium: {
+        url: "https://i.ytimg.com/vi/FWP41MXLMGY/mqdefault.jpg",
+        width: 320,
+        height: 180
+      }
+    },
+    channelTitle: "CGP Grey",
+    resourceId: {
+      videoId: "FWP41MXLMGY"
+    }
+  },
+  {
+    publishedAt: "2021-01-27T16:33:40Z",
+    title: "❄️🇬🇧 London Snow Day 🇬🇧❄️",
+    thumbnails: {
+      default: {
+        url: "https://i.ytimg.com/vi/gqMxf52CCmk/default.jpg",
+        width: 120,
+        height: 90
+      },
+      medium: {
+        url: "https://i.ytimg.com/vi/gqMxf52CCmk/mqdefault.jpg",
+        width: 320,
+        height: 180
+      }
+    },
+    channelTitle: "CGP Grey",
+    resourceId: {
+      videoId: "gqMxf52CCmk"
+    }
+  }
+];
+
+export const channelsInfo = [
+  { keyName: "cgpgrey", channelName: "CGP Grey", avatarUrl:"https://yt3.ggpht.com/ytc/AKedOLS7B0VZFxgHWsLLpXC2rHY7bflcvjVuN68OFVY6=s240-c-k-c0x00ffffff-no-rj" },
+  { keyName: "vsauce", channelName: "Vsauce", avatarUrl: "https://yt3.ggpht.com/ytc/AKedOLSCpbn6_3KOR7zAuqaS9fXtX7JAb0AQ6Y_f33ohog=s240-c-k-c0x00ffffff-no-rj" },
+  { keyName: "veritasium", channelName: "Veritasium", avatarUrl: "https://yt3.ggpht.com/ytc/AKedOLS6qhCOSn2LBAn1k1QnFYAMXAxGso7PnOeqWruerA=s240-c-k-c0x00ffffff-no-rj" },
+  { keyName: "smartereveryday", channelName: "SmarterEveryDay", avatarUrl: "https://yt3.ggpht.com/ytc/AKedOLQjGSf-ONLuBlcY6KzEFJqoaIJNjiXvM-iahXv7Cg=s240-c-k-c0x00ffffff-no-rj" },
+  { keyName: "markrober", channelName: "Mark Rober", avatarUrl: "https://yt3.ggpht.com/ytc/AKedOLRN1579qcowoOyJZgtiLI2CKMBqvHqEWFYvhrri=s240-c-k-c0x00ffffff-no-rj" },
+  { keyName: "nilered", channelName: "NileRed", avatarUrl: "https://yt3.ggpht.com/ytc/AKedOLTVNZe9V4AH3f-ttOD_Xow8M5jACEWUQ9Gd0GMs8A=s240-c-k-c0x00ffffff-no-rj" },
+  { keyName: "realengineering", channelName: "Real Engineering", avatarUrl: "https://yt3.ggpht.com/ytc/AKedOLRHtlYUfKRGpaVbGFvz8w2M8r91QToE7kk_n2Hgnw=s240-c-k-c0x00ffffff-no-rj" }
+];
+
+export const videosTestArr = () => {
+  let randomVids = [];
+  for (let i = 0; i < 15; i++) {
+    randomVids.push(videos[Math.floor(Math.random() * videos.length)]);
+  }
+  return randomVids;
+};

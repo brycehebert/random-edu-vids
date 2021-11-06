0f3b2d4d80edb047f41fa0a72ff8ec41a097a37e
Bryce Hebert <brycehebert@users.noreply.github.com>
2021-11-06 01:38:42 -0500

    OptionsPanel background

--- client/src/components/ChannelBox.js
+++ client/src/components/ChannelBox.js
@@ -5,7 +5,7 @@ const ChannelBox = ({channel}) => {
   return (
     <div className="ChannelBox">
       <Container display="flex" alignItems="center" py="2">
-        <Form.Checkbox name="channel" value={channel.keyName} display="inline" checked mx="5" />
+        <Form.Checkbox name="channel" value={channel.keyName} display="inline" defaultChecked={true} m="5" />
         <Media>
           <Media.Item align="left" display="inline-block">
             <Image src={channel.avatarUrl} size={48} rounded  />

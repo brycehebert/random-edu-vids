ede27514d40d86d83ad3e807c52cd495db87468d
Bryce Hebert <brycehebert@users.noreply.github.com>
2021-12-01 04:42:45 -0600

    Track state of checkboxes in optionsPanel

--- client/src/components/ChannelBox.js
+++ client/src/components/ChannelBox.js
@@ -1,11 +1,11 @@
 import { Container, Media, Image, Form, Heading } from "react-bulma-components";
 import "../stylesheets/ChannelBox.scss"
 
-const ChannelBox = ({channel}) => {
+const ChannelBox = ({channel, handleClick}) => {
   return (
     <div className="ChannelBox">
       <Container display="flex" alignItems="center" py="2">
-        <Form.Checkbox name="channel" value={channel.keyName} display="inline" defaultChecked={true} m="5" />
+        <Form.Checkbox name="channel" value={channel.id} display="inline" defaultChecked={true} onChange={handleClick} m="5" />
         <Media>
           <Media.Item align="left" display="inline-block">
             <Image src={channel.avatarUrl} size={48} rounded  />
--- client/src/components/OptionsPanel.js
+++ client/src/components/OptionsPanel.js
@@ -1,21 +1,45 @@
-import "../stylesheets/OptionsPanel.scss"
-import { Form, Button, Container } from "react-bulma-components"
-import ChannelBox from "./ChannelBox"
-import channelsInfo from "../channels"
+import "../stylesheets/OptionsPanel.scss";
+import { Form, Button, Container } from "react-bulma-components";
+import ChannelBox from "./ChannelBox";
+import channelsInfo from "../channels";
+import { useState } from "react";
 
 const OptionsPanel = () => {
-  let channels = channelsInfo.map((ele, index) => <ChannelBox key={index} channel={ele}/> )
+  const [selected, setSelected] = useState(channelsInfo.map((ele) => ele.id));
 
-  return (    
+  const handleCheckbox = (e) => {
+    let newSelected = [...selected];
+    let value = e.target.value;
+    let channelIndex = newSelected.findIndex((ele) => ele === value);
+
+    if (e.target.checked) {
+      if (channelIndex === -1) {
+        newSelected.push(value);
+      }
+    } else {
+      if (channelIndex !== -1) {
+        newSelected.splice(channelIndex, 1);
+      }
+    }
+    setSelected(newSelected);
+  };
+
+  let channels = channelsInfo.map((ele, index) => (
+    <ChannelBox key={index} channel={ele} handleClick={handleCheckbox} />
+  ));
+
+  return (
     <div className="OptionsPanel">
       <Form.Control>
         {channels}
-        <Container textAlign="center" tablet={{textAlign: "left"}}>
-          <Button my={2} color="info">Get New Playlist</Button>
+        <Container textAlign="center" tablet={{ textAlign: "left" }}>
+          <Button my={2} color="info">
+            Get New Playlist
+          </Button>
         </Container>
       </Form.Control>
     </div>
-  )
-}
+  );
+};
 
-export default OptionsPanel
+export default OptionsPanel;
